eb3be03049d2d6265a899b820384f967a74ae0d3
ayushrawat98 <aayushrawat0@gmail.com>
2025-03-06 08:58:53 +0530

    Angular update

--- models/sync.js
+++ models/sync.js
@@ -9,31 +9,34 @@ import Actions from "./actions.model.js"
 import { WebtorrentClient } from '../helper/webtorrent.js'
 
 
-Users.hasMany(Videos, {as : 'UserVideos', onDelete : 'CASCADE', foreignKey : 'UploaderUserId'})
-Videos.belongsTo(Users, {as : 'Uploader', foreignKey : 'UploaderUserId'})
+Users.hasMany(Videos, { as: 'UserVideos', onDelete: 'CASCADE', foreignKey: 'UploaderUserId' })
+Videos.belongsTo(Users, { as: 'Uploader', foreignKey: 'UploaderUserId' })
 
-Users.belongsToMany(Users, {through : Subscriptions, as : 'SubscribedTo', foreignKey : 'subscriber_id'})
-Users.belongsToMany(Users, {through : Subscriptions, as : 'SubscribedBy', foreignKey : 'subscribedto_id'})
+Users.belongsToMany(Users, { through: Subscriptions, as: 'SubscribedTo', foreignKey: 'subscriber_id' })
+Users.belongsToMany(Users, { through: Subscriptions, as: 'SubscribedBy', foreignKey: 'subscribedto_id' })
 
-Users.hasMany(Comments, {as : 'UserComments', onDelete : 'CASCADE', foreignKey : 'CommentorUserId'})
-Comments.belongsTo(Users, {as : 'Commentor', foreignKey : 'CommentorUserId'})
-Videos.hasMany(Comments, { as : 'VideoComments', onDelete : 'CASCADE', foreignKey : 'CommentedVideoId'})
-Comments.belongsTo(Videos, {as : 'CommentedVideo', foreignKey : 'CommentedVideoId'})
-Comments.hasMany(Comments, {as:'ChildComments',foreignKey : 'parentcommentid', onDelete : 'CASCADE'})
-Comments.belongsTo(Comments, {as:'ParentComment',foreignKey : 'parentcommentid'})
+Users.hasMany(Comments, { as: 'UserComments', onDelete: 'CASCADE', foreignKey: 'CommentorUserId' })
+Comments.belongsTo(Users, { as: 'Commentor', foreignKey: 'CommentorUserId' })
+Videos.hasMany(Comments, { as: 'VideoComments', onDelete: 'CASCADE', foreignKey: 'CommentedVideoId' })
+Comments.belongsTo(Videos, { as: 'CommentedVideo', foreignKey: 'CommentedVideoId' })
+Comments.hasMany(Comments, { as: 'ChildComments', foreignKey: 'parentcommentid', onDelete: 'CASCADE' })
+Comments.belongsTo(Comments, { as: 'ParentComment', foreignKey: 'parentcommentid' })
 
-Users.hasMany(Notifications, {as : 'UserNotifications', onDelete : 'CASCADE', foreignKey : 'NotifiedUserId'})
-Notifications.belongsTo(Users, {as : 'NotifiedUser', foreignKey : 'NotifiedUserId'})
+Users.hasMany(Notifications, { as: 'UserNotifications', onDelete: 'CASCADE', foreignKey: 'NotifiedUserId' })
+Notifications.belongsTo(Users, { as: 'NotifiedUser', foreignKey: 'NotifiedUserId' })
 
-Videos.belongsToMany(Users, {through : Actions, as : 'Likers', foreignKey : 'LikerUserId'})
-Users.belongsToMany(Videos, {through : Actions, as : 'LikedVideos', foreignKey : 'LikedVideoId'})
+Videos.belongsToMany(Users, { through: Actions, as: 'Likers', foreignKey: 'LikerUserId' })
+Users.belongsToMany(Videos, { through: Actions, as: 'LikedVideos', foreignKey: 'LikedVideoId' })
 
 
-const syncer = async () => { await SequelizeInstance.sync({}) ;
-let t = await Videos.findAll()
-t.map(x => x.toJSON()).forEach(element => {
-    WebtorrentClient.seed('./public/videos/'+ element.videoPath , async (torrent) => {})
-});
+const syncer = async () => {
+    await SequelizeInstance.sync({});
+
+    //seed the torrents on startup
+    let t = await Videos.findAll()
+    t.map(x => x.toJSON()).forEach(element => {
+        WebtorrentClient.seed('./public/videos/' + element.videoPath, async (torrent) => { })
+    });
 }
 
 export default syncer
