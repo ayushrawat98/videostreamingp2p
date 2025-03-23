32cfe1c97250d88df9b2d6171335bfcf277c3fd9
ayushrawat98 <aayushrawat0@gmail.com>
2025-03-23 10:48:47 +0530

    Thumbnail fix

--- helper/generateThumbnail.js
+++ helper/generateThumbnail.js
@@ -9,7 +9,7 @@ function generateThumbnail(path, filename, newFileName) {
     if (metadata) {
       const duration = metadata.format.duration
       const timestamps = [
-         duration / 4, duration / 2, 3*duration / 4, duration-1
+        duration / 4, duration / 2, 3*duration / 4, duration-1
       ]
         Ffmpeg(`public/videos/${path}`)
           .screenshots({
@@ -19,7 +19,7 @@ function generateThumbnail(path, filename, newFileName) {
             size: "320x180"
           })
           .on('end', () => {
-            // console.log('Thumbnail generated successfully.');
+            console.log('Thumbnail generated successfully.');
             // generateFast(path, newFileName)
           })
           .on('error', (err) => {
@@ -28,20 +28,20 @@ function generateThumbnail(path, filename, newFileName) {
     }
   })
 
-  // Ffmpeg(`public/videos/${path}`)
-  // .screenshots({
-  //   timestamps: [1], // Capture a thumbnail at 1 second into the video
-  //   filename: `${filename}.jpg`, // Generate a unique filename
-  //   folder: 'public/thumbnails',
-  //   size:"320x180"
-  // })
-  //   .on('end', () => {
-  //     // console.log('Thumbnail generated successfully.');
-  //     // generateFast(path, newFileName)
-  //   })
-  //   .on('error', (err) => {
-  //     console.error('Error generating thumbnail:', err);
-  //   });
+  Ffmpeg(`public/videos/${path}`)
+  .screenshots({
+    timestamps: [1], // Capture a thumbnail at 1 second into the video
+    filename: `${filename}.jpg`, // Generate a unique filename
+    folder: 'public/thumbnails',
+    size:"320x180"
+  })
+    .on('end', () => {
+      // console.log('Thumbnail generated successfully.');
+      // generateFast(path, newFileName)
+    })
+    .on('error', (err) => {
+      console.error('Error generating thumbnail:', err);
+    });
 
 }
 
